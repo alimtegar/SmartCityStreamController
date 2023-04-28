de90f495ea3d6480824336a891eeb335ab19d77e
alimtegar <alimtegar4@gmail.com>
2023-04-28 10:13:56 +0700

    create endpoint to add camera with uploaded video

--- app/camera/routes.py
+++ app/camera/routes.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter, Depends
+import os
+import time
+import json
+from typing import Optional
+from fastapi import APIRouter, Depends, UploadFile, File, Form, HTTPException
 from fastapi.responses import JSONResponse
 
 from app.camera.repository import CameraRepository
@@ -9,6 +13,42 @@ router = APIRouter(prefix="/camera")
 
 @router.post("/")
 def add(camera: CreateCamera, camera_repository: CameraRepository = Depends(get_camera_repository)):
+    new_camera = camera_repository.add(camera)
+    return JSONResponse(status_code=201, content={
+        "data": new_camera.dict(),
+    })
+
+
+@router.post("/video")
+def add_with_uploaded_video(
+    file: UploadFile = File(...), 
+    name: str = Form(...),
+    width: Optional[int] = Form(320),
+    loop: Optional[bool] = Form(False),
+    counter_line_str: Optional[str] = Form('[[0,0], [0, 0]]'),
+    camera_repository: CameraRepository = Depends(get_camera_repository)
+):
+    try:
+        contents = file.file.read()
+        
+        file_name, file_ext = os.path.splitext(file.filename)
+        file_name += f'-{int(time.time())}'
+        file_path = f'./uploads/videos/{file_name}{file_ext}'
+        
+        with open(file_path, 'wb') as f:
+            f.write(contents)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        file.file.close()
+    
+    camera = CreateCamera(
+        name=name,
+        source=file_path, 
+        width=width,
+        loop=loop,
+        counter_line=json.loads(counter_line_str),
+    )
     new_camera = camera_repository.add(camera)
     return JSONResponse(status_code=201, content={
         "data": new_camera.dict(),
